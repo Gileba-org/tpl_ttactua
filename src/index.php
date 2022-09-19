9618babb87c8618415d095e845c859fb8ee4d253
Gijs Lamon <info@gileba.be>
2022-09-19 22:53:24 +0200

    First take at homepage

--- src/index.php
+++ src/index.php
@@ -4,33 +4,40 @@
 	use Joomla\CMS\Factory;
 
 	$app = Factory::getApplication();
+	$menu 		= $app->getMenu();
+	$lang 		= $app->getLanguage();
 	$page = $app->getRouter()->getVars();
 	$wa = $this->getWebAssetManager();
 
-	switch ($page["option"]) {
-		case "com_content":
-			switch ($page["view"]) {
-				case "article":
-					$wa->useStyle('template.ttactua.article');
-					break;
-				case "category":
-					switch ($page["layout"]) {
-						case "blog":
-							$wa->useStyle('template.ttactua.blog');
-							break;
-						default:
-							break;
-					}
-					break;
-				case "featured":
-					$wa->useStyle('template.ttactua.featured');
-					break;
-				default:
-					break;
-			}
-			break;
-		default:
-			$wa->useStyle('template.ttactua.base');
+	if ($menu->getActive() == $menu->getDefault($lang->getTag())) {
+		$wa->useStyle('template.ttactua.contentheader');
+	}
+	else {
+		switch ($page["option"]) {
+			case "com_content":
+				switch ($page["view"]) {
+					case "article":
+						$wa->useStyle('template.ttactua.article');
+						break;
+					case "category":
+						switch ($page["layout"]) {
+							case "blog":
+								$wa->useStyle('template.ttactua.blog');
+								break;
+							default:
+								break;
+							}
+						break;
+					case "featured":
+						$wa->useStyle('template.ttactua.featured');
+						break;
+					default:
+						break;
+				}
+				break;
+			default:
+				$wa->useStyle('template.ttactua.base');
+		}
 	}
 ?>
 
@@ -47,8 +54,17 @@
 ?>
 	</head>
 	<body>
+		<div class="contentheader">
+			<jdoc:include type="modules" name="content-header" />
+		</div>
+<?php
+	if ($menu->getActive() != $menu->getDefault($lang->getTag())) :
+?>
 		<div class="component">
 			<jdoc:include type="component" />
 		</div>
+<?php
+	endif;
+?>
 	</body>
 </html>
