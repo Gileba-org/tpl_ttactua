92c6a694c0f1cfeed152e9417a36dfd5dcbd1c27
Gijs Lamon <info@gileba.be>
2022-09-19 16:58:59 +0200

    Blog + andere verfijningen

--- src/index.php
+++ src/index.php
@@ -1,9 +1,39 @@
 <?php
 	defined('_JEXEC') or die('Restricted access');
 
+	use Joomla\CMS\Factory;
+
+	$app = Factory::getApplication();
+	$page = $app->getRouter()->getVars();
 	$wa = $this->getWebAssetManager();
-	$wa->useStyle('template.ttactua.article');
+
+	switch ($page["option"]) {
+		case "com_content":
+			switch ($page["view"]) {
+				case "article":
+					$wa->useStyle('template.ttactua.article');
+					break;
+				case "category":
+					switch ($page["layout"]) {
+						case "blog":
+							$wa->useStyle('template.ttactua.blog');
+							break;
+						default:
+							break;
+					}
+					break;
+				case "featured":
+					$wa->useStyle('template.ttactua.featured');
+					break;
+				default:
+					break;
+			}
+			break;
+		default:
+			$wa->useStyle('template.ttactua.base');
+	}
 ?>
+
 <!DOCTYPE html>
 <html lang="<?php echo $this->language; ?>" >
    <head>
