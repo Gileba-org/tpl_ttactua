39065ab95c1634313c223b2b40abf262bb9803c8
Gijs Lamon <info@gileba.be>
2022-11-09 20:51:54 +0100

    Remove hidden option - view - layout debug

--- src/index.php
+++ src/index.php
@@ -75,7 +75,6 @@
 <?php	if ($this->params->get('publisherid')) {	?>
 			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-<?php echo $this->params->get('googleads'); ?>" crossorigin="anonymous"></script>
 <?php	}	?>
-		<div style="display: none">Option: <?php echo $page['option'];?>; View: <?php echo $page['view']; ?>; Layout: <?php echo $page['layout'] ?>;</div>
 		<div class="header">
 			<div class="logo">
 				<a href="" alt="Home"><img src="media/templates/site/ttactua/images/logo.jpg" /></a>
